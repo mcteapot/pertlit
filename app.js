e4fe51d911e63f569cba577b04ca1f0ed000b93b
Arjun Prakash <archieoi@gmail.com>
2012-11-29 14:43:47 -0800

    node up and running

---
+++ app.js
@@ -0,0 +1,74 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+/**
+ * Configurations.
+ */
+
+// Configuration App
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+  app.use(app.router);
+  app.use(require('less-middleware')({ src: __dirname + '/public' }));
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+
+// Configuration Development
+/*
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+*/
+app.configure('development', function(){
+  app.use(express.logger({ format: ':method :uri' }));
+  app.use(express.errorHandler());
+});
+
+app.configure('production', function() {
+  app.use(express.logger());
+  app.use(express.errorHandler()); 
+});
+
+app.configure('test', function() {
+  app.use(express.logger());
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+
+/**
+ * Route Pages.
+ */
+
+
+app.get('/', function(req, res){
+  res.render('index', { title: 'Pertlit' });
+});
+
+//app.get('/', routes.index);
+app.get('/users', user.list);
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  //console.log("Express server listening on port " + app.get('port'));
+  console.log("Express server listening on port %d in %s mode http://localhost:%d/", app.get('port'), app.settings.env, app.get('port'));
+
+});
